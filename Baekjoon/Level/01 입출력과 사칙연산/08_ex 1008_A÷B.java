c5844bf1fdc9d2e7be1aad4f98437033fb63651d
LJH-3410 <sos341000@gmail.com>
2022-05-02 22:20:55 +0900

    백준 입출력과 사칙연산

---
+++ Baekjoon/Level/01 입출력과 사칙연산/08_ex 1008_A÷B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.print(a / b);
+	}
+}
