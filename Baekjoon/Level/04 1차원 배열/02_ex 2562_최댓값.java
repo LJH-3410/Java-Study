222d0d2df3d23e9a1e1981f5ab763cc5d3be6ae9
LJH-3410 <sos341000@gmail.com>
2022-06-05 10:47:30 +0900

    빈 내용 수정

--- Baekjoon/Level/04 1차원 배열/02_ex 2562_최댓값.java
+++ Baekjoon/Level/04 1차원 배열/02_ex 2562_최댓값.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
- 
+
 public class Main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int[] arr = { in.nextInt(), in.nextInt(), in.nextInt(),
-				in.nextInt(), in.nextInt(), in.nextInt(),
-				in.nextInt(), in.nextInt(), in.nextInt() };
-		in.close();
-		
+		Scanner scan = new Scanner(System.in);
+		int[] arr = new int[9];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
 		int count = 0;
 		int max = 0;
 		int index = 0;
-        
-		for(int value : arr) {
+
+		for (int value : arr) {
 			count++;
-            
-			if(value > max) {
+
+			if (value > max) {
 				max = value;
 				index = count;
 			}
 		}
 		System.out.print(max + "\n" + index);
-		
+
 	}
 }
