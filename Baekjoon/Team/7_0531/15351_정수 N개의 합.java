e22f9cffe776182d7dca1fbd5dfaf5831e6c4165
LJH-3410 <sos341000@gmail.com>
2022-06-05 22:46:25 +0900

    내용 수정

--- Baekjoon/Team/7_0531/15351_정수 N개의 합.java
+++ Baekjoon/Team/7_0531/15351_정수 N개의 합.java
@@ -1,10 +1,38 @@
-public class Main {
-    public static void main(String[] args) {
-		long sum = 0;	
-        
-		for(int i = 0; i < a.length; i++) {
-			sum += a[i];
+package sample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Exam15352 {
+	public static void main(String[] args) {
+		BufferedReader br;
+		int count = 0;
+		String[] str = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			count = Integer.parseInt(br.readLine());
+			str = new String[count];
+
+			for (int i = 0; i < count; i++) {
+				str[i] = br.readLine();
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		return sum;
+
+		for (int i = 0; i < count; i++) {
+			int sum = 0;
+			for (int j = 0; j < str[i].length(); j++) {
+				if (str[i].charAt(j) != ' ')
+					sum += (int) str[i].charAt(j) - 64;
+			}
+			if (sum == 100)
+				System.out.println("PERFECT LIFE");
+			else
+				System.out.println(sum);
+		}
+
 	}
 }
